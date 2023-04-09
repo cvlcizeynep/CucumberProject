22650d20ea9584cd6bd92635304661016204beb7
Zeynep Cuvalci <cvlcizeynep@outlook.com>
2023-04-09 12:24:02 +0300

    09//04==2

--- src/test/java/stepdefinitions/uidefinitions/LoginStepDefinitions.java
+++ src/test/java/stepdefinitions/uidefinitions/LoginStepDefinitions.java
@@ -3,10 +3,13 @@ package stepdefinitions.uidefinitions;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.checkerframework.checker.units.qual.C;
+import org.junit.Assert;
 import pages.LoginPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+
+import static org.junit.Assert.assertEquals;
 
 public class LoginStepDefinitions {
     LoginPage loginPage=new LoginPage();
@@ -20,8 +23,62 @@ public class LoginStepDefinitions {
     public void kullanici_admin_olarak_login_olur() {
    ReusableMethods.login(ConfigReader.getProperty("adminUserName"),ConfigReader.getProperty("adminPassword"));
     }
-    @Then("Basariyla Login oldugunu dogrular.")
-    public void basariyla_login_oldugunu_dogrular() {
+
+    @Then("Basariyla admin girisi yaptigini dogrular.")
+    public void basariyla_admin_girisi_yaptigini_dogrular() {
+        ReusableMethods.waitFor(3);
+     String actualUrl =Driver.getDriver().getCurrentUrl();
+     String expectedUrl="https://www.managementonschools.com/admin-management";
+        assertEquals(expectedUrl,actualUrl);
+
+    }
+
+
+    @When("Kullanici Dean olarak login olur.")
+    public void kullanici_dean_olarak_login_olur() {
+        ReusableMethods.login(ConfigReader.getProperty("deanUserName"),ConfigReader.getProperty("deanPassword"));
+    }
+
+    @Then("Basariyla Dean girisi yaptigini  dogrular.")
+    public void basariyla_dean_girisi_yaptigini_dogrular() {
+        ReusableMethods.waitFor(3);
+        String actualUrl=Driver.getDriver().getCurrentUrl();
+        String expectedUrl="https://www.managementonschools.com/vicedean-management";
+        assertEquals(expectedUrl,actualUrl);
+    }
+    @Then("close driver")
+    public void close_driver() {
+       Driver.closeDriver();
+    }
+
+
+
+    @When("Kullanici viceDean olarak login olur.")
+    public void kullanici_vice_dean_olarak_login_olur() {
+        ReusableMethods.login(ConfigReader.getProperty("viceDeanUserName"),ConfigReader.getProperty("viceDeanPassword"));
+    }
+
+    @Then("Basariyla viceDean girisi yaptigini dogrular.")
+    public void basariyla_vice_dean_girisi_yaptigini_dogrular() {
+
+    }
+
+    @When("Kullanici Teacher olarak login olur.")
+    public void kullanici_teacher_olarak_login_olur() {
+
+    }
+    @Then("Basariyla teacher girisi yaptigini dogrular.")
+    public void basariyla_teacher_girisi_yaptigini_dogrular() {
+
+    }
+
+    @When("Kullanici Student olarak login olur.")
+    public void kullanici_student_olarak_login_olur() {
+
+    }
+    @Then("Basariyla student girisi yaptigini dogrular.")
+    public void basariyla_student_girisi_yaptigini_dogrular() {
+
 
     }
 
--- src/test/resources/features/uifeature/Login.feature
+++ src/test/resources/features/uifeature/Login.feature
@@ -1,7 +1,30 @@
+
 Feature:
   @login
   Scenario: Admin Login
     When Kullanici StudentManagement sayfasina gider.
     When Kullanici admin olarak login olur.
-    Then Basariyla Login oldugunu dogrular.
+    Then Basariyla admin girisi yaptigini dogrular.
+    Then close driver
+@login
+    Scenario: DeanLogin
+      When Kullanici StudentManagement sayfasina gider.
+      When Kullanici Dean olarak login olur.
+      Then Basariyla Dean girisi yaptigini  dogrular.
+      Then close driver
 
+
+      Scenario: ViceDeanLogin
+        When Kullanici StudentManagement sayfasina gider.
+        When Kullanici viceDean olarak login olur.
+        Then Basariyla viceDean girisi yaptigini dogrular.
+
+        Scenario:
+          When Kullanici StudentManagement sayfasina gider.
+          When Kullanici Teacher olarak login olur.
+          Then Basariyla teacher girisi yaptigini dogrular.
+
+          Scenario:
+            When Kullanici StudentManagement sayfasina gider.
+            When Kullanici Student olarak login olur.
+            Then Basariyla student girisi yaptigini dogrular.
