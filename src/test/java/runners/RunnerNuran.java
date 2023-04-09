8501ea930d07e346d4de068b9dd584e8c46876ec
Zeynep Cuvalci <cvlcizeynep@outlook.com>
2023-04-09 12:45:36 +0300

    09//04//

---
+++ src/test/java/runners/RunnerNuran.java
@@ -0,0 +1,20 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class) // test çalıştırıcı notasyonu
+@CucumberOptions( // Seneryoların nerede ve nasıl çalışacağı, hangi raporun kullanılmasıyla alakalı seçenekleri ayarlar
+        plugin = {"html:src/test/resources/reports/g1g2g3g4.html"},
+        monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "@login ",
+        dryRun = false
+        //Seneryonun adımlarını kontrol eder ve çalıştığını görürüz
+
+)
+public class RunnerNuran {
+
+}
