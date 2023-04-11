8294dc92dfc70689fed33dfbda962f475b568155
Nuran Albayrak <nuranalbayrak03@gmail.com>
2023-04-11 16:44:07 +0300

    n

--- src/test/java/pages/DeanManagementPage.java
+++ src/test/java/pages/DeanManagementPage.java
@@ -1,7 +1,50 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class DeanManagementPage {
 
     //nuran hanim,hayriye hanim
+    public DeanManagementPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//button[normalize-space()='Menu']")
+    public WebElement MenuButton;
+
+    @FindBy(xpath = "//a[normalize-space()='Dean Management']")
+    public WebElement DeanManagementButton;
+
+    @FindBy(xpath = "//input[@id='name']")
+    public WebElement NameButton;
+
+    @FindBy(xpath = "//input[@id='surname']")
+    public WebElement SurnameButton;
+    @FindBy(xpath = "//input[@id='birthPlace']")
+    public WebElement BirthplaceButton;
+    @FindBy(xpath = "//input[contains(@value,'FEMALE')]")
+    public WebElement FemaleButton;
+
+    @FindBy(xpath = "//input[@value='MALE']")
+    public WebElement MaleButton;
+    @FindBy(xpath = "//input[@id='birthDay']")
+    public WebElement DateOfBirthButton;
+    @FindBy(xpath = "//input[@id='phoneNumber']")
+    public WebElement PhoneNumberButton;
+    @FindBy(xpath = "//input[@id='ssn']")
+    public WebElement SsnButton;
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement UserNameButton;
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement PasswordButton;
+    @FindBy(xpath = "//button[normalize-space()='Submit']")
+    public WebElement SubmitButton;
+    @FindBy(xpath = "//div[contains(text(),'Dean Saved')]")
+    public WebElement DeanSavedAlert;
+
+
 
 }
