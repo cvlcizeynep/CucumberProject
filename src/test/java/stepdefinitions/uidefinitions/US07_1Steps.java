09931ce85c19a4c1e775adccd68fdaf35f93e1e5
Zeynep Cuvalci <cvlcizeynep@outlook.com>
2023-04-13 23:44:30 +0300

    13.04

---
+++ src/test/java/stepdefinitions/uidefinitions/US07_1Steps.java
@@ -0,0 +1,46 @@
+package stepdefinitions.uidefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US07_1Steps {
+
+    @When("Kullanici acilan sayfada menu kismina tiklar")
+    public void kullanici_acilan_sayfada_menu_kismina_tiklar() {
+
+    }
+    @When("Menuden contact get all u tiklar")
+    public void menuden_contact_get_all_u_tiklar() {
+
+    }
+    @Then("Dean hesabiyla mesajlarin gorulebildigini dogrular")
+    public void dean_hesabiyla_mesajlarin_gorulebildigini_dogrular() {
+
+    }
+
+    @Then("Kullanici dean hesabiyla mesaj gonderen kisilerin goruldugunu dogrular")
+    public void kullanici_dean_hesabiyla_mesaj_gonderen_kisilerin_goruldugunu_dogrular() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+    @Then("Kullanici dean hesabiyla mesaj gonderen kisilerin maillerinin goruldugunu dogrular")
+    public void kullanici_dean_hesabiyla_mesaj_gonderen_kisilerin_maillerinin_goruldugunu_dogrular() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Kullanici dean hesabiyla gonderilen mesajlarin tarihlerinin  goruldugunu dogrular")
+    public void kullanici_dean_hesabiyla_gonderilen_mesajlarin_tarihlerinin_goruldugunu_dogrular() {
+
+    }
+
+    @Then("Kullanici dean hesabiyla gonderilen mesajlarin subjectlerinin goruldugunu dogrular")
+    public void kullanici_dean_hesabiyla_gonderilen_mesajlarin_subjectlerinin_goruldugunu_dogrular() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
