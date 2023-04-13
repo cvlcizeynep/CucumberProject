29c464ff3fd83d4a31ce223995fb3866ec8980e4
kullanıcı adı / rumuz <E-posta>
2023-04-13 21:07:02 +0300

    13/04/20232

--- src/test/java/pages/MeetManagementPage.java
+++ src/test/java/pages/MeetManagementPage.java
@@ -1,5 +1,57 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class MeetManagementPage {
     //Muammer bey, Nuran hanim
+
+
+    public MeetManagementPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    
+    @FindBy(xpath = "//button[text()='Menu']")  public WebElement menu;
+    @FindBy(xpath = "//a[text()='Meet Management']") public WebElement meet_Management;
+    @FindBy(xpath = "//input[@id='date']")  public WebElement date;
+    @FindBy(xpath = "//input[@id='startTime']") public WebElement startTime;
+    @FindBy(xpath = "//input[@id='stopTime']") public WebElement stopTime;
+    @FindBy(xpath = "//input[@id='description']")   public WebElement description;
+    @FindBy(xpath = "//*[@class='table-responsive']")   public WebElement ChoseStuentds1;
+    @FindBy(id="react-select-2-input")   public WebElement ChoseStuentds2;
+    @FindBy(xpath = "//button[text()='Submit']")   public WebElement meetSubmit;
+    @FindBy(xpath = "//*[@class='text-dark btn btn-outline-info']")   public WebElement edit;
+
+
+    @FindBy(xpath = "//*[@class=' css-19bb58m']")  public WebElement editChoseStuentds1;
+    @FindBy(id="react-select-5-input")  public WebElement editChoseStuentds2;
+    @FindBy(xpath = "//input[@id='date']")  public WebElement editDate;
+    @FindBy(xpath = "//input[@id='startTime']") public WebElement editMeetStartTime;
+    @FindBy(xpath = "//input[@id='stopTime']") public WebElement editMeetStopTime;
+    @FindBy(xpath = "//input[@id='description']")   public WebElement editMeetDescription;
+    @FindBy(xpath = "//button[text()='Submit']")   public WebElement editMeetSubmit;
+    @FindBy(xpath = "//div[@role='alert']")   public WebElement editMeetBasarili;
+    @FindBy(xpath = "//div[contains(text()='JSON parse error: Cannot coerce empty  ']")   public WebElement editMeetUyariMesaji;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/stepdefinitions/uidefinitions/US8PozitiveStepdefinition.java
@@ -0,0 +1,64 @@
+package stepdefinitions.uidefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.support.PageFactory;
+import pages.LessonsManagementPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class US8PozitiveStepdefinition {
+    LessonsManagementPage lessonsManagementPage = new LessonsManagementPage();
+
+
+    @Given("Lessons a tiklar")
+    public void lessons_a_tiklar() {
+        lessonsManagementPage.lessons.click();
+    }
+    @Given("Lessonsa {string}  girer")
+    public void lessonsa_girer(String string) {
+        ReusableMethods.waitFor(5);
+        lessonsManagementPage.lessonName.sendKeys(string);
+    }
+    @Then("Lesson in {string} oldugunu dogrular")
+    public void lesson_in_oldugunu_dogrular(String string) {
+        ReusableMethods.waitFor(5);
+        Assert.assertTrue(lessonsManagementPage.lessonName.isDisplayed());
+    }
+    @And("Dersin Compulsory olup olmadığını işaretler")
+    public void dersinCompulsoryOlupOlmadığınıIşaretler() {
+        ReusableMethods.waitFor(5);
+        lessonsManagementPage.compulsory.click();
+    }
+
+    @And("Dersin Compulsory olup olmadığını işaretlendigini dogrular")
+    public void dersinCompulsoryOlupOlmadığınıIşaretlendiginiDogrular() {
+        ReusableMethods.waitFor(5);
+        Assert.assertTrue(lessonsManagementPage.compulsory.isSelected());
+    }
+
+    @Given("Credit Score {string}  girer")
+    public void credit_score_girer(String string) {
+        ReusableMethods.waitFor(5);
+        lessonsManagementPage.creditScore.sendKeys(string);
+    }
+
+    @And("Credit Score degeri girdigini dogrular")
+    public void creditScoreDegeriGirdiginiDogrular() {
+        ReusableMethods.waitFor(5);
+        Assert.assertTrue(lessonsManagementPage.creditScore.isDisplayed());
+
+    }
+
+    @Then("close the application")
+    public void closeTheApplication() {
+        Driver.quitDriver();
+    }
+
+    @And("Credit Score {string}  gırer")
+    public void creditScoreGırer(String string) {
+        lessonsManagementPage.creditScore.sendKeys("25");
+    }
+}
