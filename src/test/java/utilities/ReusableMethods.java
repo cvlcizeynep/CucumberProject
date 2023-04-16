77795744f2713485cd0d4c4a4ec2eca82c14d2ea
Fatmanur Altun <fatmanuraltun13@gmail.com>
2023-04-16 12:13:19 +0300

    16.04.23

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -221,6 +221,10 @@ public class ReusableMethods {
         }
     }
 
+    public static void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
 
 
 
