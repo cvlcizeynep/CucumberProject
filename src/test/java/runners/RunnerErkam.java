64be1d3fea2b8d77a6d3213d96e9b5b82a6d9bd2
erkam <ercome1994@gmail.com>
2023-04-19 18:20:35 +0300

    sdfdfs

--- src/test/java/runners/RunnerErkam.java
+++ src/test/java/runners/RunnerErkam.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "stepdefinitions",
-        tags = "@smokes",
+        glue = {"stepdefinitions","hooks"},
+        tags = "@25",
         dryRun = false
 )
 public class RunnerErkam {
