211998a4ba733436fc1781c95ab3bf11e81fae64
Hayriye Kılıç <kilichayriye03@gmail.com>
2023-04-30 18:51:47 +0300

    v1

--- src/test/java/stepdefinitions/dbstepdefinitions/US02Steps.java
+++ src/test/java/stepdefinitions/dbstepdefinitions/US02Steps.java
@@ -1,30 +1,37 @@
 package stepdefinitions.dbstepdefinitions;
 
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import pages.RegisterPage;
 import utilities.DBUtils;
+import utilities.Driver;
+import utilities.ReusableMethods;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class US02Steps {
+    RegisterPage registerPage =new RegisterPage();
     List<Object> namelist = new ArrayList<>();
     List<Object> phonenumberlist = new ArrayList<>();
     List<Object> ssnlist = new ArrayList<>();
     List<Object> usernamelist = new ArrayList<>();
-    List<Object> GuestUserList = new ArrayList<>();
 
-//***************************name*********************
+    //***************************name*********************
+
     @When("Admin Guest User name bilgilerini databasede sorgular")
     public void admin_guest_user_name_bilgilerini_databasede_sorgular() {
         namelist = Collections.singletonList(DBUtils.getQueryResultList("select name from guest_user"));
-        System.out.println("guest_userlist = " + namelist);
+        System.out.println("guest_user_name = " + namelist);
     }
     @Then("Admin Guest User name bilgilerini gorebildigini dogrular")
     public void admin_guest_user_name_bilgilerini_gorebildigini_dogrular() {
-        Assert.assertTrue(namelist.toString().contains("Yvonne"));
+        Assert.assertTrue(namelist.toString().contains("[hayriye]"));
 
         //**************phone number********************
 
@@ -32,11 +39,11 @@ public class US02Steps {
     @When("Admin Guest User phone number bilgilerini databasede sorgular")
     public void admin_guest_user_phone_number_bilgilerini_databasede_sorgular() {
         phonenumberlist = Collections.singletonList(DBUtils.getQueryResultList("select phone_number from guest_user"));
-        System.out.println("guest_userlist = " + phonenumberlist);
+        System.out.println("guest_userphone_number = " + phonenumberlist);
     }
     @Then("Admin Guest User phone number bilgilerini gorebildigini dogrular")
     public void admin_guest_user_phone_number_bilgilerini_gorebildigini_dogrular() {
-        Assert.assertTrue(phonenumberlist.toString().contains("[708-637-2047]"));
+        Assert.assertTrue(phonenumberlist.toString().contains("[645-969-9245]"));
 
     //**************************ssn************************
 
@@ -44,11 +51,11 @@ public class US02Steps {
     @When("Admin Guest User ssn bilgilerini databasede sorgular")
     public void admin_guest_user_ssn_bilgilerini_databasede_sorgular() {
         ssnlist= Collections.singletonList(DBUtils.getQueryResultList("select ssn from guest_user"));
-        System.out.println("guest_userlist = " + ssnlist);
+        System.out.println("guest_userssn = " + ssnlist);
     }
     @Then("Admin Guest User ssn bilgilerini gorebildigini dogrular")
     public void admin_guest_user_ssn_bilgilerini_gorebildigini_dogrular() {
-        Assert.assertTrue(ssnlist.toString().contains("745-88-2045"));
+        Assert.assertTrue(ssnlist.toString().contains("[159-31-4394]"));
 
      //***********************username*******************
 
@@ -56,19 +63,45 @@ public class US02Steps {
     @When("Admin Guest User username bilgilerini databasede sorgular")
     public void admin_guest_user_username_bilgilerini_databasede_sorgular() {
         usernamelist= Collections.singletonList(DBUtils.getQueryResultList("select username from guest_user"));
-        System.out.println("guest_userlist = " + usernamelist);
+        System.out.println("guest_user_username = " + usernamelist);
 
     }
     @Then("Admin Guest User username bilgilerini gorebildigini dogrular")
     public void admin_guest_user_username_bilgilerini_gorebildigini_dogrular() {
-        Assert.assertTrue(usernamelist.toString().contains("Canayse"));
+        Assert.assertTrue(usernamelist.toString().contains("[hayriye_kilic]"));
 
     }
 
 
     @Then("Admin Guest User username bilgilerini goremedigini dogrular")
     public void adminGuestUserUsernameBilgileriniGoremediginiDogrular() {
-        Assert.assertFalse(usernamelist.toString().contains("abcd"));
+        Assert.assertFalse(usernamelist.toString().contains("a78d"));
+
+    }
+
+    @Given("Guest user {string} {string} {string} {string}  {string} {string} {string} {string} girerek kayit olur")
+    public void guestUserGirerekKayitOlur(String name, String surname, String birthplace, String phone, String data, String ssn, String username, String password) {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        Driver.getDriver().get("http://139.59.159.36:3000/register");
+        ReusableMethods.waitFor(1);
+        registerPage.name.sendKeys(name);
+        ReusableMethods.waitFor(1);
+        registerPage.surname.sendKeys(surname);
+        ReusableMethods.waitFor(1);
+        registerPage.birthplace.sendKeys(birthplace);
+        registerPage.phonenumber.sendKeys(phone);
+        Driver.clickWithJS(registerPage.gendermale);
+        registerPage.dateofbhirt.sendKeys(data);
+        registerPage.ssn.sendKeys(ssn);
+        registerPage.username.sendKeys(username);
+        registerPage.password.sendKeys(password);
+        js.executeScript("arguments[0].click()", registerPage.registerSubmitButton);
+        ReusableMethods.waitFor(1);
+        Driver.closeDriver();
+
+
+
+
 
     }
 }
